91f68578e124ca81232f28e593b6210805320630
Job van der Zweep <jobvan-d@student.codam.nl>
2021-12-15 12:58:55 +0100

    make gnl fasta

--- src/get_next_line.c
+++ src/get_next_line.c
@@ -3,13 +3,7 @@
 #include <unistd.h> /* read */
 #include <stdlib.h> /* free */
 
-static void	m_free_and_set_null(void **p)
-{
-	free(*p);
-	*p = NULL;
-}
-
-static int	read_until_newline(const int fd, char **s)
+static int	read_until_newline(const int fd, char **s, ssize_t *len)
 {
 	char	buf[BUFFER_SIZE + 1];
 	ssize_t	bytes_read = -1;
@@ -22,6 +16,7 @@ static int	read_until_newline(const int fd, char **s)
 			return (bytes_read);
 		}
 		buf[bytes_read] = 0;
+		*len += bytes_read;
 		*s = ft_strfjoin(*s, buf);
 	}
 	return (bytes_read);
@@ -30,6 +25,7 @@ static int	read_until_newline(const int fd, char **s)
 char	*get_next_line(int fd)
 {
 	static char		*sto = NULL;
+	static ssize_t	len = 0;
 	char			*ret;
 	ssize_t			bytes_read;
 	size_t			index;
@@ -37,7 +33,7 @@ char	*get_next_line(int fd)
 	ret = NULL;
 	if (ft_strchr(sto, '\n') == 0)
 	{
-		bytes_read = read_until_newline(fd, &sto);
+		bytes_read = read_until_newline(fd, &sto, &len);
 		if (bytes_read == -1 || (bytes_read == 0 && sto == NULL))
 			return (NULL);
 	}
@@ -45,12 +41,15 @@ char	*get_next_line(int fd)
 	if (index == 0)
 	{
 		if (*sto != 0)
-			ret = ft_strndup_unsafe(sto, ft_strlen(sto));
-		m_free_and_set_null((void **)&sto);
+			ret = ft_strndup_unsafe(sto, len);
+		len = 0;
+		free(sto);
+		sto = NULL;
 		return (ret);
 	}
 	index -= ((size_t)sto);
 	ret = ft_strndup_unsafe(sto, index == 0 ? index + 1 : index);
-	ft_memcpy(sto, sto + index + 1, ft_strlen(sto) + 1 - (index + 1));
+	ft_memcpy(sto, sto + index + 1, len + 1 - (index + 1));
+	len -= index + 1;
 	return (ret);
 }
